f31405d5509da20c5cbac9ef7d4e672b50bfa7ec
haruki7049 <tontonkirikiri@gmail.com>
2024-10-15 11:06:16 +0900

    feat(repo): Init

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        { pkgs, system, ... }:
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.nixfmt.enable = true;
+            programs.yamlfmt.enable = true;
+            programs.ormolu.enable = true;
+            programs.cabal-fmt.enable = true;
+          };
+
+          devShells.default = pkgs.callPackage ./shell.nix { };
+        };
+    };
+}
